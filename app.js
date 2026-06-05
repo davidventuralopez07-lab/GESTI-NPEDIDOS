6f5f618c2ec0ce1f3a8016d147f379b0a37b717f
davidventuralopez07-lab <davidventuralopez07@gmail.com>
2026-06-05 18:07:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,126 @@ function agregarCliente() {
         alert("Completa todos los campos");
         return;
     }
+function verPedidosFecha(){
 
+    const fecha =
+    document.getElementById("fechaConsulta").value;
+
+    const resultado =
+    document.getElementById("resultadoCalendario");
+
+    resultado.innerHTML = "";
+
+    const pedidosDia =
+    pedidos.filter(p => p.fecha === fecha);
+
+    if(pedidosDia.length === 0){
+
+        resultado.innerHTML =
+        "<p>No hay pedidos para ese día.</p>";
+
+        return;
+    }
+
+    let totalDia = 0;
+
+    pedidosDia.forEach(p=>{
+
+        totalDia += p.total;
+
+        resultado.innerHTML += `
+            <div style="
+                border:1px solid #ddd;
+                padding:15px;
+                margin-top:10px;
+                border-radius:8px;
+                background:#fafafa;
+            ">
+                <strong>${p.cliente}</strong><br>
+                Total pedido: ${p.total.toFixed(2)} €
+            </div>
+        `;
+    });
+
+    resultado.innerHTML += `
+        <h3>
+            Total del día: ${totalDia.toFixed(2)} €
+        </h3>
+    `;
+}
+
+async function descargarPDF(){
+
+    const fecha =
+    document.getElementById("fechaConsulta").value;
+
+    const pedidosDia =
+    pedidos.filter(p => p.fecha === fecha);
+
+    if(pedidosDia.length === 0){
+
+        alert("No hay pedidos para ese día");
+
+        return;
+    }
+
+    const { jsPDF } = window.jspdf;
+
+    const pdf = new jsPDF();
+
+    let y = 20;
+
+    pdf.setFontSize(18);
+    pdf.text("VENTURA",10,y);
+
+    y += 10;
+
+    pdf.setFontSize(12);
+    pdf.text("Pedidos del día: " + fecha,10,y);
+
+    y += 15;
+
+    pedidosDia.forEach(p=>{
+
+        pdf.text(
+            "Cliente: " + p.cliente,
+            10,
+            y
+        );
+
+        y += 8;
+
+        p.lineas.forEach(l=>{
+
+            pdf.text(
+                `${l.producto} | ${l.kg} kg | ${l.precio} €/kg | ${l.subtotal.toFixed(2)} €`,
+                15,
+                y
+            );
+
+            y += 8;
+
+            if(y > 260){
+                pdf.addPage();
+                y = 20;
+            }
+
+        });
+
+        pdf.text(
+            "TOTAL PEDIDO: " +
+            p.total.toFixed(2) + " €",
+            15,
+            y
+        );
+
+        y += 12;
+    });
+
+    pdf.save(
+        "Pedidos_" + fecha + ".pdf"
+    );
+}
     clientes.push({
         nombre,
         direccion
