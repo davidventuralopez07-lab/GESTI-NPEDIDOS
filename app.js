8c5370b0ec301973830504b114701132501dc4cb
davidventuralopez07-lab <davidventuralopez07@gmail.com>
2026-06-05 17:37:44 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,3 +10,72 @@ function guardarProductos() {
 }
 
 console.log("Aplicación iniciada");
+function renderClientes() {
+
+    const tabla = document.getElementById("tablaClientes");
+
+    if (!tabla) return;
+
+    tabla.innerHTML = "";
+
+    clientes.forEach((cliente, index) => {
+
+        tabla.innerHTML += `
+            <tr>
+                <td>${cliente.nombre}</td>
+                <td>${cliente.direccion}</td>
+                <td>
+                    <button class="delete" onclick="eliminarCliente(${index})">
+                        X
+                    </button>
+                </td>
+            </tr>
+        `;
+    });
+}
+
+function agregarCliente() {
+
+    const nombre = document.getElementById("nombreCliente").value;
+    const direccion = document.getElementById("direccionCliente").value;
+
+    if (!nombre || !direccion) {
+        alert("Completa todos los campos");
+        return;
+    }
+
+    clientes.push({
+        nombre,
+        direccion
+    });
+
+    guardarDatos();
+    renderClientes();
+
+    document.getElementById("nombreCliente").value = "";
+    document.getElementById("direccionCliente").value = "";
+}
+
+function eliminarCliente(index) {
+
+    clientes.splice(index, 1);
+
+    guardarDatos();
+    renderClientes();
+}
+
+function borrarTodosClientes() {
+
+    if (!confirm("¿Seguro que quieres borrar todos los clientes?")) {
+        return;
+    }
+
+    clientes = [];
+
+    guardarDatos();
+    renderClientes();
+}
+
+window.onload = function() {
+    renderClientes();
+};
